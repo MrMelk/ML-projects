ab9261ea7aadaf94e9a77c88bfcf8eb31acde0e4
Are Tov Kjelkenes Sløtte <aretov@live.com>
2020-12-29 19:01:06 +0100

    batch commit due to refresher simplicity

---
+++ Refresher/persisting_model.py
@@ -0,0 +1,17 @@
+#beginners persisting model
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.externals import joblib
+
+music_data = pd.read_csv("music.csv")
+X = music_data.drop(columns = ["genre"])
+y = music_data["genre"]
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+joblib.dump(model, "music-recommender.joblib")
+joblib.load("music-recommender.joblib")
+
+predictions = model.predict([[21, 1]])
+predictions
