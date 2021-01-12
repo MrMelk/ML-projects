c09dfe5bb603555749c7aa5d97c2e727b40c9f30
MrMelk <54366653+MrMelk@users.noreply.github.com>
2021-01-12 13:50:12 +0100

    Update to movierec.py

--- Movie Recommendations/movierec.py
+++ Movie Recommendations/movierec.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[18]:
 
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from scipy.stats import zscore
 
 # Hva vil du modellen skal gjøre:
 # Basert på tags og genres skal modellen finne hva slags filmer en bruker har 
@@ -26,13 +27,13 @@ ratings = pd.read_csv("ratings.csv")
 tags = pd.read_csv("tags.csv")
 
 
-# In[20]:
+# In[3]:
 
 
 print(tags.shape, genomeS.shape, genomeT.shape, link.shape, movies.shape, ratings.shape)
 
 
-# In[19]:
+# In[11]:
 
 
 genres = []
@@ -57,20 +58,51 @@ df = tags.merge(newMovies, on="movieId")
 newRatings = ratings.drop(columns = "timestamp")
 df.drop(columns = ["timestamp", "tag", "title"], inplace = True)
 df = df.sort_values("userId")
+df
 
 
-# In[18]:
+# In[38]:
 
 
 
 #splitte opp dataframen min til #userId dataframes og lage
+df = df.reset_index(drop = True)
 
 def modelMakerId(df):
     models = []
     X = df.drop(columns = "movieId")
     y = df["movieId"]
     users = df["userId"]
-    for user in df["userId"]:
-        
-    
+    for i in range(df.shape[0]):
+        if df[df["userId"] == i]:
+            X = df[df["userId"] == i].drop(columns = ["movieId"])
+            y = df[df["userId"] == i].drop(columns = ["userId"])
+            train_test_split(X, y, test_size = 0.2)
+            
+            model = RandomForestClassifier()
+            model.fit(X, y)
+            models.append(["userId " + str(i), model])
+        #ta alle som er true når user == start user, lag modell, send inn resten
+        #bruke df[df["userId"] == x]?
+        #this currently does not work, will attempt later
+    return models
+
+
+X = df[df["userId"] == 14].drop(columns = ["movieId"])
+y = df[df["userId"] == 14]["movieId"]
+X = X.to_numpy()
+y = y.to_numpy()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+pred = model.predict(X_test)
+score = accuracy_score(y_test, pred)
+score
+
+
+# In[ ]:
+
+
+
 
